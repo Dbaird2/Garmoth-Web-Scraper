958fc04f433400a880beffc5cbc4d18ec2b9ef80
Dbaird2 <161085580+Dbaird2@users.noreply.github.com>
2026-04-26 14:46:15 -0600

    removed padding

--- Frontend/web-scrape-frontend/pages/ItemPage.jsx
+++ Frontend/web-scrape-frontend/pages/ItemPage.jsx
@@ -16,7 +16,7 @@ export default function ItemPage() {
     <>
       <title>Event Tracker: Items</title>
       <div
-        className="min-h-screen p-6 md:p-10 bg-[#090e10]"
+        className="min-h-screen bg-[#090e10]"
         style={{
           backgroundImage:
             "linear-gradient(rgba(0,200,180,0.03) 1px, transparent 1px), linear-gradient(90deg, rgba(0,200,180,0.03) 1px, transparent 1px)",
